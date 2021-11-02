767a0b960185c408a122f3aac4a3cb301276efbc
Lhaíslla Cavalcanti <lhaislla@gmail.com>
2021-11-02 11:47:59 -0300

    Add files via upload

---
+++ Dart/maps.dart
@@ -0,0 +1,9 @@
+main() {
+  Map<String, dynamic> maps = {
+    "nome": "Lhaíslla Cavalcanti",
+    "idade": 23,
+    "cidade": "Carpina",
+    "estado": "Pernambuco"
+  };
+  print(maps["nome"]);
+}
